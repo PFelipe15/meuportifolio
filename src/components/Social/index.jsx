a391f5c628a3bf0a90e288fd9aaf4c77814f1bce
Paulo Felipe <PauloFelipet15@gmail.com>
2023-01-24 20:51:17 -0300

    links changes

--- src/components/Social/index.jsx
+++ src/components/Social/index.jsx
@@ -4,7 +4,7 @@ import './social.css'
 function Social() {
   return (
     <div className="social-container">
-      <a target={"_blank"} href="https://www.linkedin.com/in/paulo-felipe-torres-ara%C3%BAjo-45337723b/"> <BsLinkedin size={35} color="#fff" /></a>
+      <a target={"_blank"} href="https://www.linkedin.com/in/pauloofer/"> <BsLinkedin size={35} color="#fff" /></a>
       <a target={"_blank"} href="https://www.instagram.com/paullofelipe_/"> <BsInstagram size={35} color="#fff" /></a>
       <a target={"_blank"} href="https://github.com/PFelipe15"> <BsGithub size={35} color="#fff" /></a>
       
